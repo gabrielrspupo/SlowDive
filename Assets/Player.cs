a769a45b429d51856337b492ce3c8336eac2a38f
Gabriel Pupo <gabrielrspupo@gmail.com>
2017-11-09 21:12:35 -0200

    Movimentação do jogador e pulo duplo

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,30 +5,59 @@ using UnityEngine;
 public class Player : MonoBehaviour {
     //Make sure that we can see class in Inspector
     [System.Serializable]
-    public class PlayerStats
-    {
+    public class PlayerStats {
         public float Health = 100f;
 
     }
 
     PlayerStats playerStats = new PlayerStats();
     public float fallBoundary = -15;
+	public float speed = 5;
+	public float jumpImpulse = 10;
+	private int jumpLimit = 1;
+	private Rigidbody2D rb2d;
+	private SpriteRenderer sr;
 
-    public void DamagePlayer(int damage)
-    {
+	void Start() {
+		rb2d = GetComponent<Rigidbody2D>();
+		sr = GetComponent<SpriteRenderer>();
+	}
+
+    public void DamagePlayer(int damage) {
         playerStats.Health -= damage;
-        if(playerStats.Health <= 0)
-        {
+        if(playerStats.Health <= 0) {
             Debug.Log("Kill Player");
             GameManager.KillPlayer(this);
         }
     }
 
-    void Update()
-    {
-        if (transform.position.y <= fallBoundary)
-        {
+	void OnCollisionEnter2D (Collision2D collision) {
+		jumpLimit = 1;
+	}
+
+	void OnCollisionExit2D (Collision2D collision) {
+		jumpLimit = 0;
+	}
+
+    void Update() {
+        if (transform.position.y <= fallBoundary) {
             DamagePlayer(int.MaxValue);
-        } 
+        }
     }
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		Vector2 pos = transform.position;
+
+		if (Input.GetKeyDown ("up") && jumpLimit >= 0) {
+			rb2d.AddForce (new Vector2 (0, jumpImpulse), ForceMode2D.Impulse);
+			jumpLimit--;
+		} else {
+			rb2d.AddForce (new Vector2 (moveHorizontal, 0) * speed);
+			if (moveHorizontal < 0)
+				sr.flipX = true;
+			else
+				sr.flipX = false;
+		}
+	}
 }
